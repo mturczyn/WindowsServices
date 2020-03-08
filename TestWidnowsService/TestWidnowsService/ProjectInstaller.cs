94df18f791db708421641456de8bf4cb16dfdae3
MichalWindows <michal.turczyn@imobit.pl>
2020-03-08 11:29:12 +0100

    Pierwszy komit: notatki, solucja

---
+++ TestWidnowsService/TestWidnowsService/ProjectInstaller.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using TestWidnowsService.Properties;
+
+namespace TestWidnowsService
+{
+  [RunInstaller(true)]
+  public partial class ProjectInstaller : System.Configuration.Install.Installer
+  {
+    public ProjectInstaller()
+    {
+      InitializeComponent();
+      File.AppendAllText(@"C:\users\michal\desktop\serviceFile.txt", $"initialize comp: {Settings.Default.StartMode}\n");
+      this.serviceInstaller1.StartType = Settings.Default.StartMode;
+      this.serviceProcessInstaller1.Account = Settings.Default.ServiceAccount;
+      this.serviceProcessInstaller1.Username = Settings.Default.User;
+      this.serviceProcessInstaller1.Password = Settings.Default.Password;
+    }
+    /// <summary>
+    /// W trakcie instalacji zmiany do instalatorów nie mają skutku. Trzeba to zrobić w InitializaComponent. Ale tam z kolei nie mamy dostępu do 
+    /// parametrów z wierza poleceń.
+    /// </summary>
+    /// <param name="stateSaver"></param>
+    public override void Install(IDictionary stateSaver)
+    {
+      base.Install(stateSaver);
+      File.AppendAllText(@"c:\users\michal\desktop\servicefile.txt", "Przed słownikiem\n");
+      var enumerator = stateSaver.Keys.GetEnumerator();
+      while (enumerator.MoveNext())
+        File.AppendAllText(@"c:\users\michal\desktop\servicefile.txt", "słownik: " + enumerator.Current.ToString());
+      File.AppendAllText(@"c:\users\michal\desktop\servicefile.txt", "po słowniku\n");
+
+      File.AppendAllText(@"c:\users\michal\desktop\servicefile.txt", "zaczynamy instalację usługi\n");
+    }
+    /// <summary>
+    /// Metoda pobierające wskazany parametr z lini poleceń, dostarczony jako [nazwa]=[wartość]
+    /// </summary>
+    /// <param name="p"></param>
+    /// <returns></returns>
+    private object GetParam(string p)
+    {
+      try
+      {
+        if (this.Context != null && this.Context.Parameters != null)
+        {
+          return this.Context.Parameters[p];
+        }
+      }
+      catch
+      {
+      }
+      return null;
+    }
+  }
+}
---
+++ TestWidnowsService/TestWidnowsService/Service1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using TestWidnowsService.Properties;
+
+namespace TestWidnowsService
+{
+  public partial class Service1 : ServiceBase
+  {
+    private Timer _timer;
+    public Service1()
+    {
+      InitializeComponent();
+    }
+
+    protected override void OnStart(string[] args)
+    {
+      _timer = new Timer(1000);
+      _timer.Elapsed += (s, e) =>
+      {
+        File.AppendAllText(@"C:\users\michal\desktop\serviceFile.txt", DateTime.Now.ToString() + "\n");
+      };
+      _timer.Start();
+      File.AppendAllText(@"C:\users\michal\desktop\serviceFile.txt", Directory.GetCurrentDirectory() + "\n");
+      File.AppendAllText(@"C:\users\michal\desktop\serviceFile.txt", Settings.Default.TextToWriteToFile + "\n");
+
+      //try
+      //{
+      //  throw new Exception();
+      //}
+      //catch
+      //{
+      //  File.AppendAllText(@"C:\users\michal\desktop\serviceFile.txt", "Błąd w usłudze zatrzymywanie.\n");
+      //  Stop();
+      //}
+    }
+
+    protected override void OnStop()
+    {
+      base.OnStop();
+      File.AppendAllText(@"C:\users\michal\desktop\serviceFile.txt", "Stop usłgui\n");
+      _timer?.Stop();
+      _timer?.Dispose();
+    }
+  }
+}
